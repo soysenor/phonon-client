c0d1f9b4bee927b7d7b2b98c44689847cb0487c7
Daniel Veenstra <dan.veenstra@gridplus.io>
2022-02-03 17:26:07 -0600

    starting to develop ChainService interface

--- chain/chain.go
+++ chain/chain.go
@@ -12,18 +12,26 @@ var ErrMissingPubKey = errors.New("phonon missing pubKey")
 var ErrMissingKeyIndex = errors.New("phonon missing KeyIndex")
 var ErrUnknownCurrencyType = errors.New("unknown currency type")
 
+type ChainService interface {
+	DeriveAddress(p *model.Phonon) (address string, err error)
+}
+
 /*Check the phonon's currency type and public key and returns a chain specific
 address as a hexstring*/
 func DeriveAddress(p *model.Phonon) (address string, err error) {
 	switch p.CurrencyType {
 	case model.Ethereum:
-		return deriveETHAddress(p.PubKey)
+		//TODO initialize this elsewhere
+		eth := &ETHChainService{}
+		return eth.DeriveAddress(p.PubKey)
 	default:
 		return "", ErrUnknownCurrencyType
 	}
 }
 
+type ETHChainService struct{}
+
 //Derives an ETH address from a phonon's ECDSA Public Key
-func deriveETHAddress(pubKey *ecdsa.PublicKey) (address string, err error) {
+func (eth *ETHChainService) DeriveAddress(pubKey *ecdsa.PublicKey) (address string, err error) {
 	return ethcrypto.PubkeyToAddress(*pubKey).Hex(), nil
 }
